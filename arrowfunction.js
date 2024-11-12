6f4e8e452a3abea814f5c411e175876a81b61db8
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-11-12 16:04:51 +0530

    Create arrowfunction.js

---
+++ arrowfunction.js
@@ -0,0 +1,16 @@
+var f1 = ()=>{
+    console.log(" Arrow Function executed....");
+   }
+   f1();
+
+var f2 = (message)=>{
+    console.log(""+message);
+   }
+   f2("Wishing you");
+
+var f3 = message=>{
+     console.log(""+message);
+     f2("start of vikram samwat")
+   }
+
+   f3("Happy Diwali....");
