d20cd9eb66512b98709fe572cb2c33fb9603fc1d
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-11-18 20:18:31 +0530

    Create _1pattern.js

---
+++ _1pattern.js
@@ -0,0 +1,16 @@
+<script>
+  for(let i=1;i<=5;i++)
+{
+    let a=""
+
+    for(let k=1;k<=5-i;k++)
+{
+    a+=" "
+}
+    for(let j=1;j<=i;j++)
+    {
+        a+="*"
+    }
+    console.log(a)
+}
+</script>
