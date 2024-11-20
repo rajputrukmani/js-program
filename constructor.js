c435b1bc8842413043ff6c781d34694d308a7803
rajputrukmani <169047459+rajputrukmani@users.noreply.github.com>
2024-11-20 22:02:03 +0530

    Create constructor.js

---
+++ constructor.js
@@ -0,0 +1,14 @@
+<script>
+    // Addition [a,b,add()] 
+    class Addition{
+        constructor(a,b){
+            this.a = a;
+            this.b = b;
+        }
+        add(){
+            console.log("Addition : "+(this.a + this.b));
+        }
+    }
+    let obj = new Addition(20,10);
+    obj.add();
+</script>
